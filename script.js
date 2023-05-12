6515aa12b9238d25f8eb1502c9dfe836e763a99f
aidanm85 <122405734+aidanm85@users.noreply.github.com>
2023-05-12 17:15:05 -0400

    changed from google charts to charts.js

--- script.js
+++ script.js
@@ -82,32 +82,18 @@ function loadList(){
   }
 }
 
-// load current chart package
-google.charts.load("current", {
-  packages: ["corechart", "line"]
-});
-// set callback function when api loaded
-google.charts.setOnLoadCallback(drawChart);
+const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-function drawChart() {
-  // create data object with default value
-  let data = google.visualization.arrayToDataTable([
-    ['Time', 'CPU Usage', 'RAM'],
-    [0, 0, 0],
-  ]);
-  // create options object with titles, colors, etc.
-  let options = {
-    title: "CPU Usage",
-    hAxis: {
-      textPosition: 'none',
-    },
-    vAxis: {
-      title: "Usage"
-    }
-  };
-  // draw chart on load
-  let chart = new google.visualization.LineChart(
-    document.getElementById("chart_div")
-  );
-  chart.draw(data, options);
-}
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor:"rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options:{...}
+});
