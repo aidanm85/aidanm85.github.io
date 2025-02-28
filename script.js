b354106a11a936600489ec1a62939fe78981f63a
aidanm85 <acm220@pitt.edu>
2025-02-27 19:19:46 -0500

    calls mm api

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function getAccessToken() {
         acc[parts[0]] = decodeURIComponent(parts[1]);
         return acc;
     }, {});
-    console.log("got access token!");
     return hash.access_token;
 }
 
@@ -31,10 +30,26 @@ async function getCurrentlyPlaying() {
     if (response.ok) {
         const data = await response.json();
         document.getElementById("track").innerText = `Now Playing: ${data.item.name} by ${data.item.artists.map(artist => artist.name).join(", ")}`;
+        getLyrics(data.item.name, data.item.artists[0].name);
     } else {
         document.getElementById("track").innerText = "No track is currently playing.";
     }
 }
+
+async function getLyrics(name, artist){
+    const options = {method: 'GET'};
+    const API_KEY = secrets.MM_API_KEY;
+    const url = `https://api.musixmatch.com/ws/1.1/track.search?q_track=${encodeURIComponent(name)}&q_artist=${encodeURIComponent(artist)}&apikey=${apiKey}`;
+    fetch(url, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+    
+}
+
+
+
 
 // Run on page load
 window.onload = getCurrentlyPlaying;
